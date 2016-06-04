fcd28e4087fffc8adfd54d4905dcf193fe215e0f
Cris Stringfellow <humanarity@gmail.com>
2016-06-04 18:57:20 +0800

    Completed rough sketch of all computation code for convex hulls, areas and centroids of polygons.

--- api/geofencesearch.py
+++ api/geofencesearch.py
@@ -12,6 +12,8 @@ from google.appengine.api import search
     2. When searching for a latitude and longitude we return all territories whose centre of operations ( as defined by the centroid in the polygon of the territory corners ), is within the maximum radius of a territory. So this means that the territories so returned, are not guaranteed to intersect with the given point, tho they are guaranteed to be within ( or more strictly, to have their centres within ) MAX_TERRITORY_RADIUS of the given point. 
 """
 
+import graham_scan
+
 MAX_TERRITORY_RADIUS_M = 250
 
 def convertWGS84ToGoogleBing( geopt ):
@@ -46,7 +48,7 @@ def convert_geopoints_to_planar( geopt_corners ):
 def convert_planars_to_geopoints( planars ):
   return [ convertGoogleBingToWGS84( p ) for p in planars ] 
 
-def compute_graham_scan( geopt_corners ):
+def compute_graham_scan( planars ):
   """
     Attempt to clean the data,
     and simplify it to an approximate shape
@@ -56,24 +58,34 @@ def compute_graham_scan( geopt_corners ):
 
     It attempts this using the Graham Scan
   """
-  pass
+  return graham_scan.convex_hull( planars )
 
-def compute_area( geopt_corners ):
+def compute_area( planars ):
   """
-    Compute the signed polygon area
-  """
-  pass
+    Compute the polygon area, using the Shoelace Formula
 
-def compute_centroid( geopt_corners ):
+    We remove the absolute value to allow the signed area to occur. 
+
+    Tho this will likely never be the case since our points
+    are ordered in CCW direction.
+
+    Taken from here: http://stackoverflow.com/a/24468019
+  """
+  n = len(planars) # of planar coordinates 
+  area = 0.0
+  for i in range(n):
+    j = (i + 1) % n
+    area += planars[i][0] * planars[j][1]
+    area -= planars[j][0] * planars[i][1]
+  area = area / 2.0
+  return area
+
+def compute_centroid( planars ):
   """
     Use the centroid formula to compute the centroid
-    of the polygon defined by these geopt_corners.
+    of the polygon defined by these planar co-ordinate pairs.
 
-    Limitations:
-
-    We make no attempt to account for the fake that this polygon
-    may be self intersecting, while the formula is said to 
-    only apply to non self intersecting polygons.
+    Taken from here: http://stackoverflow.com/a/14115494
 
     Additional notes:
 
@@ -85,21 +97,26 @@ def compute_centroid( geopt_corners ):
 
     This guarantees scalability and fast query speed and straightforward implementation. The cost of this method is accuracy as described above. It seems a reasonable cost, especially considering that retrieving neighbouring territories is also likely useful. A second important cost is that all territories input by providers must be less than the maximum radius. This radius determines the looseness of the groups. The smaller the radius, the tighter the groups, the larger the radius, the looser the groups returned in a query of a given point. The smaller it is, the tighter the groups of results obtained. Yet it is also a trade off, as the larger it is, the larger the territories that can be input. A workable choice can be decided by balancing these two considerations given knowledge of the real world uses, requirements and nature of the territory database.  
   """
-  lat = 0.0
-  long = 0.0
-  
-  
 
-  return ( lat, long )
+  # compute centroid 
 
-def compute_radius( centroid, geopt_corners ):
-  """
-    Find the distance between the centroid and the furthest corner.
-    Return that as the radius, all other corners are then 
-    guaranteed to be within that circle centered on the centroid
-    having that radius.
-  """
-  return 0.0
+  area = compute_area(planars)
+  imax = len(planars) - 1
+
+  cx = 0.0
+  cy = 0.0
+
+  for i in range(0,imax):
+      cx += (planars[i][0] + planars[i+1][0]) * ((planars[i][0] * planars[i+1][1]) - (planars[i+1][0] * planars[i][1]))
+      cy += (planars[i][1] + planars[i+1][1]) * ((planars[i][0] * planars[i+1][1]) - (planars[i+1][0] * planars[i][1]))
+
+  cx += (planars[imax][0] + planars[0][0]) * ((planars[imax][0] * planars[0][1]) - (planars[0][0] * planars[imax][1]))
+  cy += (planars[imax][1] + planars[0][1]) * ((planars[imax][0] * planars[0][1]) - (planars[0][0] * planars[imax][1]))
+
+  cx /= (area * 6.0)
+  cy /= (area * 6.0)
+
+  return [ cx, cy ]
 
 def create_geojson( props_dict, geopt_corners ):
   return json.dumps( {
